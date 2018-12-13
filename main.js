412b8737dcbdca63f146de4015759f6e3958b480
momo <1445107381@qq.com>
2018-12-13 22:14:09 +0800

    update js

--- main.js
+++ main.js
@@ -8,20 +8,21 @@ $(function(){
         var position = index*(-300)+'px'
         $('.images').css('transform','translateX('+position+')')
         
-    })
+    }).eq(0).click()
     $('.window').hover(function(){
         if(timeId){
             clearInterval(timeId)
         }
     },function(){
-         timeId = setInterval(function(){
-            position = index*(-300)+'px'
-            $('.images').css('transform','translateX('+position+')')
-            $('.buttons button').eq(index).addClass('active').siblings().removeClass('active')
-            index++
-            if(index===$num.length){
-                index =0
-            }
-        },2000)
-    })
+            timeId = setInterval(function(){
+                position = index*(-300)+'px'
+                $('.images').css('transform','translateX('+position+')')
+                $('.buttons button').eq(index).addClass('active').siblings().removeClass('active')
+                index++
+                if(index===$num.length){
+                    index =1
+                }
+            },2000)
+         
+    }).trigger('mouseleave')
 })
